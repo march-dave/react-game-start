1bbaf017814d337b67f167f247310770d3958616
march-dave <ysangy@gmail.com>
2017-11-12 18:27:12 -0500

    First init

---
+++ webpack.config.dev.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: ['react-hot-loader/patch', './demo/index'],
+    output: {
+        path: __dirname,
+        filename: 'bundle.js',
+        publicPath: '/',
+    },
+    module: {
+        loaders: [{
+                test: /\.js$/,
+                loaders: ['babel-loader'],
+                include: [path.join(__dirname, 'src'), path.join(__dirname, 'demo')],
+            },
+            {
+                test: /\.json$/,
+                loaders: ['json-loader'],
+            },
+            {
+                test: /\.css$/,
+                include: [path.join(__dirname, 'src'), path.join(__dirname, 'demo')],
+                loader: 'style-loader!css-loader!postcss-loader',
+            },
+        ],
+    },
+};
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: ['./demo/index'],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/',
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production'),
+            },
+        }),
+        new webpack.optimize.UglifyJsPlugin(),
+    ],
+    module: {
+        loaders: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader!postcss-loader',
+            },
+        ],
+    },
+};
