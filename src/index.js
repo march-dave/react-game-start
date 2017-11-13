a3ee6bc72d26a336e085ea7f2417c951426f95db
march-dave <ysangy@gmail.com>
2017-11-13 04:41:53 -0500

    Entry Page

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Loop, Stage } from 'react-native-kit';
+
+class componentName extends Component {
+    render() {
+        return (
+            <div>
+                <Loop>
+                    <Stage>
+
+                    </Stage>
+                </Loop>
+            </div>
+        );
+    }
+}
+
+export default componentName;
