07e9e81cd9b85de4f55fb424485ef7f20fb9a203
march-dave <ysangy@gmail.com>
2017-11-14 21:19:43 -0500

    stage test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import { Loop, Stage } from 'react-game-kit';
 
+import Hello from '../src/components/Hello';
+
 class App extends Component {
     render() {
         return (
             <div>
                 <Loop>
                     <Stage width={1024} height={576}>
-                            
+                        <Hello />
                     </Stage>
-                </Loop>
-
-                <h1>Hello World</h1>
+                </Loop>                
             </div>
         );
     }
---
+++ src/components/Hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+    render() {
+        return (
+            <div>
+                <World>
+                    <h1>Start</h1>
+                </World>
+            </div>
+        );
+    }
+}
+
+export default Hello;
