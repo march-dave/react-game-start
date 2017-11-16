9b84e57e57bf181ede9865ccc11b172eccb2da90
march-dave <ysangy@gmail.com>
2017-11-16 04:40:22 -0500

    up to in the morning

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -15,13 +15,13 @@ class Hello extends Component {
             <Loop>
                 <Stage width={width} height={height} style={{ background: '#3a9bdc' }}>
                     <WorldComponent gravity={{ x: 0, y: gravity }}>
-                        <Wall args={[0, 0, 800, 5]} />
+                        {/* <Wall args={[0, 0, 800, 5]} />
                         <Wall args={[0, 0, 5, 800]} />
                         <Wall args={[800 - 5, 0, 5, 800]} />
                         <Wall args={[0, 800 - 5, 800, 5]} />
                         <Rectangle args={[200, 100, 60, 60]} frictionAir={0.001} />
                         <Rectangle args={[400, 100, 60, 60]} frictionAir={0.05} />
-                        <Rectangle args={[600, 100, 60, 60]} frictionAir={0.1} />
+                        <Rectangle args={[600, 100, 60, 60]} frictionAir={0.1} /> */}
                     </WorldComponent>
                 </Stage>
 
