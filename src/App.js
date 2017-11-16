5c5781c2ad8d001e47c9840aec61fe6a95b6f2dc
march-dave <ysangy@gmail.com>
2017-11-16 04:31:21 -0500

    Use WorldComponent

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import { Loop, Stage } from 'react-game-kit';
 import Hello from '../src/components/Hello';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
-            <div>
-                <Loop>
-                    <Stage width={1024} height={576}>
-                        <Hello />
-                    </Stage>
-                </Loop>                
-            </div>
+            <Loop>
+                <Stage width={1024} height={576}>
+                    {/* <Hello /> */}
+                    <ChildComponent />
+                </Stage>
+            </Loop>
         );
     }
 }
@@ -21,19 +25,19 @@ export default App;
 
 class ChildComponent extends React.Component {
     static contextTypes = {
-      loop: PropTypes.object,
+        loop: PropTypes.object,
     };
-  
+
     update = () => {
-      // tick logic
-      <h1>Hello Start2</h1>
+        // tick logic
+        <h1>Hello Start2</h1>
     };
-  
+
     componentDidMount() {
-      this.context.loop.subscribe(this.update);
+        this.context.loop.subscribe(this.update);
     }
-  
+
     componentWillUnmount() {
-      this.context.loop.unsubscribe(this.update);
+        this.context.loop.unsubscribe(this.update);
     }
-  }
+}
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-import { Loop, Stage, World } from 'react-game-kit';
+import { Loop, Stage, WorldComponent } from 'react-game-kit';
 
 class Hello extends Component {
+    static defaultProps = {
+        width: 800,
+        height: 800,
+    };
+    state = {
+        gravity: 1
+    }
+
     render() {
         return (
             <Loop>
-                <Stage>
-                    <World>
-                        <div>Start</div>
-                    </World>
+                <Stage width={width} height={height} style={{ background: '#3a9bdc' }}>
+                    <WorldComponent gravity={{ x: 0, y: gravity }}>
+                        <Wall args={[0, 0, 800, 5]} />
+                        <Wall args={[0, 0, 5, 800]} />
+                        <Wall args={[800 - 5, 0, 5, 800]} />
+                        <Wall args={[0, 800 - 5, 800, 5]} />
+                        <Rectangle args={[200, 100, 60, 60]} frictionAir={0.001} />
+                        <Rectangle args={[400, 100, 60, 60]} frictionAir={0.05} />
+                        <Rectangle args={[600, 100, 60, 60]} frictionAir={0.1} />
+                    </WorldComponent>
                 </Stage>
+
             </Loop>
         );
     }
