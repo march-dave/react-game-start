77c59554194020238c97a13f4008316bbdba3796
march-dave <ysangy@gmail.com>
2017-11-26 11:50:09 -0500

    format change

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -8,24 +8,23 @@ class Hello extends Component {
         super(props);
 
         const { keys, store } = this.props;
-        
     }
 
     static propTypes = {
         keys: PropTypes.object,
         onEnterBuilding: PropTypes.func,
         store: PropTypes.object,
-    };
+      };
 
     static contextTypes = {
         loop: PropTypes.object,
-    };
+      };
 
-    move = (body, x) => {
+      move = (body, x) => {
         Matter.Body.setVelocity(body, { x, y: 0 });
-    };
-
-    update = () => {
+      };
+    
+      update = () => {
         const { body } = this.body;
         const { keys, store } = this.props;
         // console.log('body: ', body);
@@ -33,43 +32,39 @@ class Hello extends Component {
         console.log('keys.LEFT');
         // console.log(keys(a));
         if (keys.isDown(keys.LEFT)) {
-            // console.log('keys.LEFT');
-            //   this.move(body, -5);
-            // } else if (keys.isDown(keys.RIGHT)) {
-            //   this.move(body, 5);
+        // console.log('keys.LEFT');
+        //   this.move(body, -5);
+        // } else if (keys.isDown(keys.RIGHT)) {
+        //   this.move(body, 5);
         }
-    };
-
-    componentDidMount() {
+      };
+    
+      componentDidMount() {
         this.context.loop.subscribe(this.update);
-    }
-
-    componentWillUnmount() {
+      }
+    
+      componentWillUnmount() {
         this.context.loop.unsubscribe(this.update);
-    }
+      }
 
     render() {
         return (
             // <World onInit={this.physicsInit}>
-            <
-            World >
-            <
-            Body args = {
-                [0, 384, 64, 64, { inertia: Infinity }] }
-            ref = {
-                (b) => { this.body = b; } } >
-            <
-            Sprite repeat = { true }
-            src = "assets/character-sprite.png"
-            scale = { this.context.scale * 2 }
-            state = { 0 }
-            steps = {
-                [9, 9, 0, 4, 5] }
-            /> <
-            /Body>
+            <World>
+                <Body
+                    args={[0, 384, 64, 64, { inertia: Infinity }]}
+                    ref={(b) => { this.body = b; }}
+                >
+                    <Sprite
+                        repeat={true}
+                        src="assets/character-sprite.png"
+                        scale={this.context.scale * 2}
+                        state={0}
+                        steps={[9, 9, 0, 4, 5]}
+                    />
+                </Body>
 
-            <
-            /World >
+            </World >
         );
     }
 }
